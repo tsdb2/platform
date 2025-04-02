09561e0e3719810e43ddc92c379b37f3bec73b1e
lord-pando <alberto@tsdb2.com>
2025-04-02 12:46:39 +0200

    Improve the `JsonField` matcher.

--- json/json_testing.h
+++ json/json_testing.h
@@ -2,8 +2,10 @@
 #define __TSDB2_JSON_JSON_TESTING_H__
 
 #include <ostream>
+#include <type_traits>
 #include <utility>
 
+#include "absl/strings/escaping.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "json/json.h"
@@ -26,34 +28,66 @@ namespace json {
 //
 //   EXPECT_THAT(obj, AllOf(JsonField<kField1>(42), JsonField<kField2>(true)));
 //
-template <char const field_name[], typename Inner>
-class JsonFieldMatcher {
+template <char const field_name[], typename Object>
+class JsonFieldMatcher;
+
+template <char const field_name[], typename... Fields>
+class JsonFieldMatcher<field_name, tsdb2::json::Object<Fields...>>
+    : public ::testing::MatcherInterface<tsdb2::json::Object<Fields...> const&> {
  public:
   using is_gtest_matcher = void;
 
-  explicit JsonFieldMatcher(Inner inner) : inner_(std::move(inner)) {}
+  using Field = typename tsdb2::json::Object<Fields...>::template FieldType<field_name>;
 
-  // We can't provide meaningful implementations for our Describe methods because we won't know the
-  // type of the field until `MatchAndExplain` is called.
-  void DescribeTo(std::ostream* const /*os*/) const {}
-  void DescribeNegationTo(std::ostream* const /*os*/) const {}
+  template <typename Inner>
+  explicit JsonFieldMatcher(Inner&& inner)
+      : inner_(::testing::SafeMatcherCast<Field>(std::forward<Inner>(inner))) {}
 
-  template <typename... Fields>
+  ~JsonFieldMatcher() = default;
+
+  void DescribeTo(std::ostream* const os) const override {
+    *os << "whose field \"" << absl::CEscape(field_name) << "\" ";
+    inner_.DescribeTo(os);
+  }
+
+  void DescribeNegationTo(std::ostream* const os) const override {
+    *os << "whose field \"" << absl::CEscape(field_name) << "\" ";
+    inner_.DescribeNegationTo(os);
+  }
+
   bool MatchAndExplain(tsdb2::json::Object<Fields...> const& value,
-                       ::testing::MatchResultListener* const listener) const {
-    *listener << "whose field \"" << field_name << "\" ";
-    return ::testing::SafeMatcherCast<
-               typename tsdb2::json::Object<Fields...>::template FieldType<field_name>>(inner_)
-        .MatchAndExplain(value.template cget<field_name>(), listener);
+                       ::testing::MatchResultListener* const listener) const override {
+    *listener << "whose field \"" << absl::CEscape(field_name) << "\" ";
+    return inner_.MatchAndExplain(value.template cget<field_name>(), listener);
   }
 
  private:
-  Inner inner_;
+  JsonFieldMatcher(JsonFieldMatcher const&) = delete;
+  JsonFieldMatcher& operator=(JsonFieldMatcher const&) = delete;
+  JsonFieldMatcher(JsonFieldMatcher&&) = delete;
+  JsonFieldMatcher& operator=(JsonFieldMatcher&&) = delete;
+
+  ::testing::Matcher<Field> inner_;
 };
 
 template <char const field_name[], typename Inner>
-JsonFieldMatcher<field_name, Inner> JsonField(Inner inner) {
-  return JsonFieldMatcher<field_name, Inner>(std::move(inner));
+class JsonFieldImpl {
+ public:
+  explicit JsonFieldImpl(Inner&& inner) : inner_(std::move(inner)) {}
+
+  template <typename Object>
+  operator ::testing::Matcher<Object>() const {  // NOLINT(google-explicit-constructor)
+    return ::testing::Matcher<Object>(
+        new JsonFieldMatcher<field_name, std::decay_t<Object>>(inner_));
+  }
+
+ private:
+  std::decay_t<Inner> inner_;
+};
+
+template <char const field_name[], typename Inner>
+auto JsonField(Inner&& inner) {
+  return JsonFieldImpl<field_name, Inner>(std::forward<Inner>(inner));
 }
 
 }  // namespace json
